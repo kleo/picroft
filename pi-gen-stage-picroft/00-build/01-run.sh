1930c54030761b9bb111671c9c6e17a0066325f7
Kleo Bercero <kbeflo@gmail.com>
2021-11-25 23:39:50 +0800

    chore: install verbose [skip ci]

--- pi-gen-stage-picroft/00-build/01-run.sh
+++ pi-gen-stage-picroft/00-build/01-run.sh
@@ -10,15 +10,21 @@ install -m 0644 "files/etc/mycroft/mycroft.conf" "${ROOTFS_DIR}/etc/mycroft/mycr
 install -m 0644 -o "${FIRST_USER_NAME}" "files/home/pi/.bashrc" "${ROOTFS_DIR}/home/pi/.bashrc"
 install -m 0644 -o "${FIRST_USER_NAME}" "files/home/pi/AIY-asound.conf" "${ROOTFS_DIR}/home/pi/AIY-asound.conf"
 install -m 0644 -o "${FIRST_USER_NAME}" "files/home/pi/version" "${ROOTFS_DIR}/home/pi/version"
+install -v -d -m 0755 "${ROOTFS_DIR}/etc/mycroft"
+install -v -m 0644 files/etc/mycroft/mycroft.conf "${ROOTFS_DIR}/etc/mycroft/mycroft.conf"
 
-install -m 0755 -o "${FIRST_USER_NAME}" "files/home/pi/audio_setup.sh" "${ROOTFS_DIR}/home/pi/audio_setup.sh"
-install -m 0755 -o "${FIRST_USER_NAME}" "files/home/pi/auto_run.sh" "${ROOTFS_DIR}/home/pi/auto_run.sh"
-install -m 0755 -o "${FIRST_USER_NAME}" "files/home/pi/custom_setup.sh" "${ROOTFS_DIR}/home/pi/custom_setup.sh"
-install -m 0755 -o "${FIRST_USER_NAME}" "files/home/pi/update.sh" "${ROOTFS_DIR}/home/pi/update.sh"
+install -v -m 0644 -o "${FIRST_USER_NAME}" files/home/pi/.bashrc "${ROOTFS_DIR}/home/pi/.bashrc"
+install -v -m 0644 -o "${FIRST_USER_NAME}" files/home/pi/AIY-asound.conf "${ROOTFS_DIR}/home/pi/AIY-asound.conf"
+install -v -m 0644 -o "${FIRST_USER_NAME}" files/home/pi/version "${ROOTFS_DIR}/home/pi/version"
 
-install -d -m 0755 -o "${FIRST_USER_NAME}" "${ROOTFS_DIR}/home/pi/bin"
-install -m 0755 -o "${FIRST_USER_NAME}" "files/home/pi/bin/mycroft-setup-wizard" "${ROOTFS_DIR}/home/pi/bin/mycroft-setup-wizard"
-install -m 0755 -o "${FIRST_USER_NAME}" "files/home/pi/bin/mycroft-wipe" "${ROOTFS_DIR}/home/pi/bin/mycroft-wipe"
+install -v -m 0755 -o "${FIRST_USER_NAME}" files/home/pi/audio_setup.sh "${ROOTFS_DIR}/home/pi/audio_setup.sh"
+install -v -m 0755 -o "${FIRST_USER_NAME}" files/home/pi/auto_run.sh "${ROOTFS_DIR}/home/pi/auto_run.sh"
+install -v -m 0755 -o "${FIRST_USER_NAME}" files/home/pi/custom_setup.sh "${ROOTFS_DIR}/home/pi/custom_setup.sh"
+install -v -m 0755 -o "${FIRST_USER_NAME}" files/home/pi/update.sh "${ROOTFS_DIR}/home/pi/update.sh"
+
+install -v -d -m 0755 -o "${FIRST_USER_NAME}" "${ROOTFS_DIR}/home/pi/bin"
+install -v -m 0755 -o "${FIRST_USER_NAME}" files/home/pi/bin/mycroft-setup-wizard "${ROOTFS_DIR}/home/pi/bin/mycroft-setup-wizard"
+install -v -m 0755 -o "${FIRST_USER_NAME}" files/home/pi/bin/mycroft-wipe "${ROOTFS_DIR}/home/pi/bin/mycroft-wipe"
 
 on_chroot << EOF
 sudo -u pi -E git clone --verbose --progress --depth 1 --branch ${BRANCH} https://github.com/MycroftAI/mycroft-core /home/pi/mycroft-core
