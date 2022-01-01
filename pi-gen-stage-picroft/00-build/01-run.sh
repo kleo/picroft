8a6d3131616e9789b5f4d64fb4bce1ce15a88f6a
Kleo Bercero <kbeflo@gmail.com>
2022-01-01 15:31:56 +0800

    test: bash -x find unary operator error

--- pi-gen-stage-picroft/00-build/01-run.sh
+++ pi-gen-stage-picroft/00-build/01-run.sh
@@ -31,5 +31,5 @@ chown -Rv pi:pi /home/pi
 chmod -Rv +x /home/pi/*.sh
 
 sudo -u pi -E git clone --verbose --progress --depth 1 --branch ${BRANCH} https://github.com/MycroftAI/mycroft-core /home/pi/mycroft-core
-sudo -u pi -E bash /home/pi/mycroft-core/dev_setup.sh
+sudo -u pi -E bash -x /home/pi/mycroft-core/dev_setup.sh
 EOF
